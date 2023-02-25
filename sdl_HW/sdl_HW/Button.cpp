e17a5e5c8f880182b18fe76863d1392da5955043
Emischerin <e.mischerin@gmail.com>
2023-02-25 22:46:30 +0300

    Started working on button

---
+++ sdl_HW/sdl_HW/Button.cpp
@@ -0,0 +1,14 @@
+#include "Button.h"
+
+Button::Button()
+{
+	_r = 146;
+	_g = 7;
+	_b = 173;
+	_a = 1;
+}
+
+void Button::ImplBehaviour()
+{
+
+}
--- sdl_HW/sdl_HW/Button.h
+++ sdl_HW/sdl_HW/Button.h
@@ -4,9 +4,16 @@
 class Button : public Control
 {
 public:
-	Button(int x, int y);
+	Button();
+	
+
+	void ImplBehaviour() override;
+	
+
 private:
-	int _mouse_over_r, _mouse_over_g;
-	int _mouse_over_b, _mouse_over_a;
+	int _mouse_over_r = 58, _mouse_over_g = 6;
+	int _mouse_over_b = 69, _mouse_over_a = 1;
+	int _width = 45;
+	int _height = 20;
 };
 
