8eef062a695885a9e1c42701fd794cbc7f30f7dc
Emischerin <e.mischerin@gmail.com>
2023-02-26 20:34:23 +0300

    Added pointer to parent window to Header

--- sdl_HW/sdl_HW/Header.cpp
+++ sdl_HW/sdl_HW/Header.cpp
@@ -1,11 +1,12 @@
 #include "Header.h"
 
-Header::Header(int rel_y,int buttons_width)
+Header::Header(SDL_Window* w,int rel_y,int buttons_width)
 {
 	_rel_y = rel_y;
 	_buttons_width = buttons_width;
 	_border_line.SetRelativeHeight(rel_y);
 	_close_button.SetWidthAndHeight(rel_y - 1, buttons_width);
+	_parent_window = w;
 
 }
 
--- sdl_HW/sdl_HW/Header.h
+++ sdl_HW/sdl_HW/Header.h
@@ -6,14 +6,16 @@
 class Header : public Control
 {
 public:
-	Header(int rel_y,int buttons_width);
+	Header(SDL_Window* w,int rel_y,int buttons_width);
 
 	void ImplBehaviour() override;
 
 private:
 	CloseButton _close_button;
 	BorderLine _border_line;
-	int rel_y = 0;
+	int _rel_y = 0;
 	int _buttons_width = 0;
+	SDL_Window* _parent_window = nullptr;
 };
+
 
