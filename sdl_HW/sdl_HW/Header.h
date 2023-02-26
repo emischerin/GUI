5653fb1730fa44f9025563fb8753cdc512f5e2bf
Emischerin <e.mischerin@gmail.com>
2023-02-26 16:59:01 +0300

    Header assembling

--- sdl_HW/sdl_HW/Header.h
+++ sdl_HW/sdl_HW/Header.h
@@ -2,9 +2,11 @@
 #include "Control.h"
 #include "BorderLine.h"
 #include "CloseButton.h"
+
 class Header : public Control
 {
 private:
-
+	CloseButton _close_button;
+	BorderLine _border_line;
 };
 
