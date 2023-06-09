5460dade78d04b3520e087559bcedcfb27d2797d
emischerin <e.mischerin@gmail.com>
2023-06-09 10:29:55 +0300

    tracking my_to_sdl

--- sdl_HW/sdl_HW/WindowTracker.cpp
+++ sdl_HW/sdl_HW/WindowTracker.cpp
@@ -3,9 +3,15 @@
 void WindowTracker::TrackWindow(SDL_Window* sdl_w, Window* my_w)
 {
 	_sdl_to_my[sdl_w] = my_w;
+	_my_to_sdl[my_w] = sdl_w;
 }
 
 Window* WindowTracker::GetMyWindow(SDL_Window* w)
 {
 	return _sdl_to_my[w];
+}
+
+SDL_Window* WindowTracker::GetSDLWindow(Window* w)
+{
+	return _my_to_sdl[w];
 }
--- sdl_HW/sdl_HW/WindowTracker.h
+++ sdl_HW/sdl_HW/WindowTracker.h
@@ -12,7 +12,9 @@ class WindowTracker
 public:
 	void TrackWindow(SDL_Window* sdl_w, Window* my_w);
 	Window* GetMyWindow(SDL_Window* w);
+	SDL_Window* GetSDLWindow(Window* w);
 private:
 	std::map<SDL_Window*, Window*> _sdl_to_my;
+	std::map<Window*, SDL_Window*> _my_to_sdl;
 };
 
