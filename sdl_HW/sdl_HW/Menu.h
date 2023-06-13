74127d884689939de9351093dc221c086306a0d3
emischerin <e.mischerin@gmail.com>
2023-06-13 13:42:12 +0300

    working on menu

--- sdl_HW/sdl_HW/Menu.h
+++ sdl_HW/sdl_HW/Menu.h
@@ -1,8 +1,23 @@
 #pragma once
+#ifndef MENU_H
+#define MENU_H
 #include "Control.h"
 
 class Menu : public Control
 {
+public:
 
+
+	enum MenuPosType
+	{
+		LEFT,
+		RIGHT,
+		TOP,
+		BOTTOM
+	};
+
+	Menu(Control* parent);
 };
+
+#endif
 
