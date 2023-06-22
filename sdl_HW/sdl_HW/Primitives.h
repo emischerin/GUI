65fc09d29fdeec2c9472067606dd810d818dcdbc
emischerin <e.mischerin@gmail.com>
2023-06-22 16:48:56 +0300

    Primitives ctors

--- sdl_HW/sdl_HW/Primitives.h
+++ sdl_HW/sdl_HW/Primitives.h
@@ -5,7 +5,31 @@
 class Primitive
 {
 public:
-		 
+	
+	Primitive(int x, int y, int w, int h)
+	{
+		_bounding_rect.x = x;
+		_bounding_rect.y = y;
+		_bounding_rect.w = w;
+		_bounding_rect.h = h;
+	}
+
+	Primitive(SDL_Rect* bounding_rect)
+	{
+		_bounding_rect.x = bounding_rect->x;
+		_bounding_rect.y = bounding_rect->y;
+		_bounding_rect.w = bounding_rect->w;
+		_bounding_rect.h = bounding_rect->h;
+	}
+
+	Primitive(SDL_Rect&& bounding_rect)
+	{
+		_bounding_rect.x = bounding_rect.x;
+		_bounding_rect.y = bounding_rect.y;
+		_bounding_rect.w = bounding_rect.w;
+		_bounding_rect.h = bounding_rect.h;
+	}
+
 	virtual void ReactToEvents() {};
 	virtual void Draw() = 0;
 
@@ -16,6 +40,7 @@ protected:
 	* This is for solid one color figures
 	*/
 	SDL_Color _color = { 0,0,0,1 };
+	SDL_Rect _bounding_rect;
 
 
 	
