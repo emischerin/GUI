1084b78f3931705160aee588f79889cbddd2ff7c
emischerin <e.mischerin@gmail.com>
2023-11-27 17:39:27 +0300

    + new Collision API

--- sdl_HW/sdl_HW/CollisionDetector.h
+++ sdl_HW/sdl_HW/CollisionDetector.h
@@ -15,9 +15,16 @@ public:
 
 	bool PointYInRect(SDL_Rect* rect, int x, int y);
 
+
+
+
 	bool AllControlsXInRect(std::vector<Control*> v, SDL_Rect* rect);
 
 	bool AllControlsYInRect(std::vector<Control*> v, SDL_Rect* rect);
 
+
+	bool AllControlsXInControl(std::vector<Control*> v, Control* c);
+
+	bool AllControlsYInControl(std::vector<Control*> v, Control* c);
 };
 
