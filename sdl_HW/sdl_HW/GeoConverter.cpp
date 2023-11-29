558239945f40afae1018b45320845e8b9287db02
emischerin <e.mischerin@gmail.com>
2023-11-29 16:07:57 +0300

    GetRect points impl

--- sdl_HW/sdl_HW/GeoConverter.cpp
+++ sdl_HW/sdl_HW/GeoConverter.cpp
@@ -2,6 +2,32 @@
 
 std::vector<SDL_Point> GeoConverter::GetAllRectPoints(SDL_Rect* rect)
 {
+	
+	std::vector<SDL_Point> result;
+
+	if (!rect) return result;
+
+	SDL_Point p1;
+	p1.x = rect->x;
+	p1.y = rect->y;
+
+	SDL_Point p2;
+	p2.x = rect->x + rect->w;
+	p2.y = rect->y;
+
+	SDL_Point p3;
+	p3.x = rect->x;
+	p3.y = rect->y + rect->h;
+
+	SDL_Point p4;
+	p4.x = rect->x + rect->w;
+	p4.y = rect->y + rect->h;
+
+	result.push_back(p1);
+	result.push_back(p2);
+	result.push_back(p3);
+	result.push_back(p4);
+
 
 }
 
